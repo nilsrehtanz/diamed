bbc57f4e99d555752bdf9374e4088242c87a4ebf
Nils Rehtanz <ge35xex@mytum.dege35xex@mytum.de>
2023-07-09 16:32:36 +0200

    made navbar responsive

--- components/layout/NavigationBar.tsx
+++ components/layout/NavigationBar.tsx
@@ -4,22 +4,52 @@ import classes from "./NavigationBar.module.css";
 import { useRouter } from "next/router";
 
 const NavigationBar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  const handleShowNavbar = () => {
+    console.log(currentRoute);
+    setShowNavbar(!showNavbar);
+  };
+
+  const disappearNavBar = () => {
+    setShowNavbar(false);
+  };
+
   return (
     <nav className={classes.navbar}>
       <div className={classes.container}>
-        <Link href="/">
-          <img src="diamedlogo.svg" alt="Logo" />
-        </Link>
-        <div className={classes.CTA}>
-          <Link href="/learn" className={classes.item}>
-            Learn
+        <div
+          className={classes.logo}
+          onClick={disappearNavBar}
+        >
+          <Link href="/">
+            <img src="diamedlogo.svg" alt="Logo" />
           </Link>
-          <Link href="/login" className={classes.item}>
-            Sign In
-          </Link>
-          <Link href="/register" className={classes.item}>
-            Register Kit
-          </Link>
+        </div>
+        <div
+          className={classes.menu_icon}
+          onClick={handleShowNavbar}
+        >
+          <img src="list.svg" alt="" />
+        </div>
+        <div
+          className={`${classes.nav_elements}  ${
+            showNavbar ? classes.active : ""
+          }`}
+        >
+          <ul>
+            <li onClick={disappearNavBar}>
+              <Link href="/learn">Learn</Link>
+            </li>
+            <li onClick={disappearNavBar}>
+              <Link href="/login">Sign In</Link>
+            </li>
+            <li onClick={disappearNavBar}>
+              <Link href="/register">Register Kit</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
