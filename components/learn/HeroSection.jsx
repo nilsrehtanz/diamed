8d1acdf79b159d3ca8adfc1cdb67cdcfe6e3d94f
Nils Rehtanz <ge35xex@mytum.dege35xex@mytum.de>
2023-07-17 13:58:13 +0200

    fixed learn page

--- components/learn/HeroSection.jsx
+++ components/learn/HeroSection.jsx
@@ -8,21 +8,24 @@ function HeroSection() {
         href="/learn/quantum"
         className={classes.quantum}
       >
-        <h2>Quantum</h2>
+        <div className={classes.quantum}>
+          <h2>Quantum</h2>
+        </div>
       </Link>
 
       <div className={classes.symptomsAndTreatments}>
-        <Link
-          href="/learn/symptoms"
-          className={classes.symptoms}
-        >
-          <h2>Symptoms</h2>
+        <Link href="/learn/symptoms">
+          <div className={classes.symptoms}>
+            <h2>Symptoms</h2>
+          </div>
         </Link>
         <Link
           href="/learn/treatments"
           className={classes.treatments}
         >
-          <h2>Treatments</h2>
+          <div className={classes.treatments}>
+            <h2>Treatments</h2>
+          </div>
         </Link>
       </div>
     </div>
